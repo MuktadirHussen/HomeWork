c5ffa9a6d8ffcda774ec03a568cd954b7e96feff
MuktadirHussen <muktdrhssn@gmail.com>
2022-02-16 17:28:29 -0500

    02162022 5:28pm

--- src/hw11Q2Polymorphism/Sister.java
+++ src/hw11Q2Polymorphism/Sister.java
@@ -1,12 +1,12 @@
 package hw11Q2Polymorphism;
 
 public class Sister {
-	
+
 	public void sister() {
 		System.out.println("This is a void type method from Sister class");
 	}
-	public void sister(int age1, int age2, int age3, String age4, int age5, int age6){
-		int total1= age1 + age2+ age3+ Integer.parseInt(age4) + age5 + age6;
+	public void sister(int age1, int age2, int age3, String age4, int age5, int age6) {
+		int total1 = age1 + age2 + age3 + Integer.parseInt(age4) + age5 + age6;
 		System.out.println("Total age of Sister: " + total1);
 	}
 	public int sister(int age1, int age2, String age4) {
@@ -15,17 +15,18 @@ public class Sister {
 		return total2;
 	}
 	// method is overloading in line 12 and line 19 because they both have,
-	// different type of parameter/signature and in different combination (line 12 has String age).
-	//final method can overload.
+	// different type of parameter/signature and in different combination (line 12
+	// has String age).
+	// final method can overload.
 	public final int sister(int age3, int age5, int age6) {
-		int total3 =age3 + age5 + age6;
+		int total3 = age3 + age5 + age6;
 		System.out.println("Total age of Sister: " + total3);
 		return total3;
 	}
-    // static method can overload.
-	public static int sister(int age2, int age5, int age6,String age4) {
+	// static method can overload.
+	public static int sister(int age2, int age5, int age6, String age4) {
 		int total4 = age2 + age5 + age6 + Integer.parseInt(age4);
 		System.out.println("Total age of Sister: " + total4);
 		return total4;
-}
+	}
 }
